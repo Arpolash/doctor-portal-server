688f5d4b2373313ecbfaf21277c143187e52f067
arpolash <Arpolas2019@gmail.com>
2020-10-11 09:09:29 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+app.use(bodyParser.json());
+app.use(cors())
+const port = 5000
+require('dotenv').config()
+
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sjfoa.mongodb.net/${process.env.DB_NAME}retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology : true });
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+
+client.connect(err => {
+  const appointmentCollection = client.db(`${process.env.DB_NAME}`).collection(`${process.env.DB_COLLECTION}`);
+    app.post('/addAppointment',(req,res) =>{
+        const appointment = req.body;
+        appointmentCollection.insertOne(appointment)
+        .then(result =>{
+            res.send(result.insertedCount > 0)
+        })
+    });
+    app.post('/appointmentsByDate',(req,res) =>{
+      const date = req.body;
+      appointmentCollection.find({date : date.date})
+      .toArray((err,document) =>{
+        res.send(document)
+      })
+  })
+});
+
+
+app.listen(process.env.PORT || port)
